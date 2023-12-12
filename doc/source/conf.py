2eea786817ff0d1219c925317e3babbc3c3732e3
BrianOBlanton <Brian_Blanton@Renci.Org>
2023-12-11 20:42:26 -0500

    more doc deploy testing

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,7 +24,7 @@ extensions = [
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = []
+exclude_patterns = ['build']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
